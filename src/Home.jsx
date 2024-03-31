ae853ce583948bf25b76e93b49caa651ee87ca4d
bbhitec <bboy.hitec@gmail.com>
2024-04-01 02:26:57 +0300

    fix: minor loading message note

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,7 +8,7 @@ const Home = ({backendUrl: url} = props) => {
         <div className="home">
 
             {/* using logical check for a conditional rendering */}
-            {isLoading && <div>Loading Blogs...</div>}
+            {isLoading && <div>Loading Blogs... <br /> (May take a while on cold launch)</div>}
             {errorMessage && <div>{errorMessage}</div>}
 
             {/* also, use this logical check to protect the inner .map from async blogs list */}
